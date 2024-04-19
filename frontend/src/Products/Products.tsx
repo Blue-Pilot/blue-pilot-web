2bf71b0dbbbbb7758f1ac182d6721cd219d8d413
Zach Gyurko <classicpizzazach@gmail.com>
2024-04-19 00:46:27 -0400

    completed 'Fixing the React Key Binding Issue'

--- frontend/src/Products/Products.tsx
+++ frontend/src/Products/Products.tsx
@@ -34,7 +34,7 @@ function Products() {
         <div className = "content">
             <ul className = "products">
                 {products.map((product) => (
-                    <li>
+                    <li key = {product.id}>
                         <div className="product">
                             <img
                                 className="product-image"
